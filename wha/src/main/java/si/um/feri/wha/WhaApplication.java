f10e4eae53a4c011193eec7b869f946f17762e04
RokFonovic <rok.fonovic@student.um.si>
2023-12-07 15:43:02 +0100

    Popravek v datotekah pom.xml in aplication.properties

--- wha/src/main/java/si/um/feri/wha/WhaApplication.java
+++ wha/src/main/java/si/um/feri/wha/WhaApplication.java
@@ -3,12 +3,11 @@ package si.um.feri.wha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Test za commit
-
 @SpringBootApplication
 public class WhaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WhaApplication.class, args);
 	}
 
