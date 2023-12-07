1e365a24a52bce77150a9037dea44da9f928eaee
RokFonovic <rok.fonovic@student.um.si>
2023-12-07 11:38:01 +0100

    Springboot projekt wha

---
+++ wha/src/test/java/si/um/feri/wha/WhaApplicationTests.java
@@ -0,0 +1,13 @@
+package si.um.feri.wha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
