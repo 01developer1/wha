188cf703f111f90482c2bb904459acb6e375c2aa
RokFonovic <rok.fonovic@student.um.si>
2023-12-18 14:39:58 +0100

    Znova ustvarjen git projekt

---
+++ wha/src/main/java/si/feri/um/wha/WhaApplication.java
@@ -0,0 +1,13 @@
+package si.feri.um.wha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhaApplication.class, args);
+	}
+
+}
