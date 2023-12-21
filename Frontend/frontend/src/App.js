661b07d4e4006302f975dceb9870244848f68ec5
RokFonovic <rok.fonovic@student.um.si>
2023-12-21 14:39:52 +0100

    ustvarjen react projekt

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Routing from './components/routing/Routing';
+import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+       <BrowserRouter>
+        <Routing />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
