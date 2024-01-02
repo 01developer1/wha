9ba28cfc0d3b540c86e20b50ba3d7cb2a9e92fb8
carluka <carluka33@gmail.com>
2024-01-02 15:02:09 +0100

    Artikel: implementacija izpisa enega zapisa (GET - glede na ID), brisanje podatkov zapisa (DELETE), kompleksnejša poizvedba z vsaj 2 parametroma #WHA-18 DONE Time 1h #WHA-28 DONE Time 1h #WHA-31 DONE Time 1h

--- wha/src/main/java/si/feri/um/wha/controllers/ArtikelController.java
+++ wha/src/main/java/si/feri/um/wha/controllers/ArtikelController.java
@@ -1,10 +1,12 @@
 package si.feri.um.wha.controllers;
 
+import org.springframework.http.ResponseEntity;
 import si.feri.um.wha.dao.ArtikelRepository;
 import si.feri.um.wha.models.Artikel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import si.feri.um.wha.models.Zaposleni;
+import si.feri.um.wha.models.Tip_artikla;
+
 
 @RestController
 @RequestMapping("/artikli")
@@ -22,5 +24,32 @@ public class ArtikelController {
     public Artikel dodajArtikel(@RequestBody Artikel artikel){
         return artikelDao.save(artikel);
     }
+
+    @GetMapping("/{ID_artikla}")
+    public Artikel vrniDolocenArtikel(@PathVariable(name = "ID_artikla") Long ID_artikel){
+        return artikelDao.vrniDolocenArtikel(ID_artikel);
+    }
+
+    @DeleteMapping("/izbrisi/{ID_artikla}")
+    public ResponseEntity<String> izbrisiArtikel(@PathVariable(name = "ID_artikla") Long ID_artikel) throws Exception {
+        artikelDao.deleteById(ID_artikel);
+        return ResponseEntity.ok("Uspesno izbrisan artikel.");
+    }
+
+
+
+    @GetMapping("/search") //vrne Artikle,ki imajo enak tip in ceno visjo od vnesene
+    public Iterable<Artikel> vrniArtikleCenaTip(
+            @RequestParam(name = "tipArtikla", required = false) String tipArtikla,
+            @RequestParam(name = "prodajnaCena", required = false) Double prodajnaCena
+    ) {
+        Tip_artikla tipArtiklaEnum = (tipArtikla != null) ? Tip_artikla.valueOf(tipArtikla) : null;
+        return artikelDao.poisciGledeNaCenoTip(tipArtiklaEnum, prodajnaCena);
+    }
+
+
+
+
+
 
 }
--- wha/src/main/java/si/feri/um/wha/dao/ArtikelRepository.java
+++ wha/src/main/java/si/feri/um/wha/dao/ArtikelRepository.java
@@ -1,10 +1,24 @@
 package si.feri.um.wha.dao;
 
+import org.springframework.data.repository.query.Param;
 import si.feri.um.wha.models.Artikel;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import si.feri.um.wha.models.Tip_artikla;
 
 
 public interface ArtikelRepository extends CrudRepository<Artikel, Long> {
+
+    @Query("select a from Artikel a where a.ID_artikel = :id")
+    Artikel vrniDolocenArtikel(@Param("id") Long id);
+
+    @Query("SELECT a FROM Artikel a WHERE (:tipArtikla IS NULL OR a.tip_artikla = :tipArtikla) AND (:prodajnaCena IS NULL OR a.prodajnaCena > :prodajnaCena)")
+    Iterable<Artikel> poisciGledeNaCenoTip(
+            @Param("tipArtikla") Tip_artikla tipArtikla,
+            @Param("prodajnaCena") Double prodajnaCena
+    );
+
+
+
 
 }
