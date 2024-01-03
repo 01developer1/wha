4715d2b43f16918a5f235c4568fd5486dfe34062
Aljaz <aljaz.kodric1@student.um.si>
2024-01-03 13:50:31 +0100

    get-id in delete delovni_cas

--- wha/src/main/java/si/feri/um/wha/controllers/Delovni_CasController.java
+++ wha/src/main/java/si/feri/um/wha/controllers/Delovni_CasController.java
@@ -1,6 +1,7 @@
 package si.feri.um.wha.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import si.feri.um.wha.dao.Delovni_CasRepository;
 import si.feri.um.wha.models.Delovni_Cas;
@@ -20,5 +21,16 @@ public class Delovni_CasController {
     @PostMapping
     public Delovni_Cas dodajDelovni_Cas(@RequestBody Delovni_Cas delovni_cas){
         return delovni_casRepositoryDao.save(delovni_cas);
+    }
+
+    @GetMapping("/{ID_delovni_cas}")
+    public Delovni_Cas vrniDolocenDelovniCas(@PathVariable(name = "ID_delovni_cas") Long ID_delovni_cas){
+        return delovni_casRepositoryDao.vrniDolocenDelovniCas(ID_delovni_cas);
+    }
+
+    @DeleteMapping("/izbrisi/{ID_delovni_cas}")
+    public ResponseEntity<String> izbrisiDelovniCas(@PathVariable(name = "ID_delovni_cas") Long ID_delovni_cas) throws Exception {
+        delovni_casRepositoryDao.deleteById(ID_delovni_cas);
+        return ResponseEntity.ok("Uspesno izbrisan delovni_cas.");
     }
 }
--- wha/src/main/java/si/feri/um/wha/dao/Delovni_CasRepository.java
+++ wha/src/main/java/si/feri/um/wha/dao/Delovni_CasRepository.java
@@ -2,7 +2,12 @@ package si.feri.um.wha.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import si.feri.um.wha.models.Delovni_Cas;
+import si.feri.um.wha.models.Zaposleni;
 
 public interface Delovni_CasRepository extends CrudRepository<Delovni_Cas, Long>{
+
+    @Query("select d from Delovni_Cas d where d.ID_delovni_cas = :id")
+    Delovni_Cas vrniDolocenDelovniCas(@Param("id") Long id);
 }
