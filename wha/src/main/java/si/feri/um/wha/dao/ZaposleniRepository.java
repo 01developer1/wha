00974cbd4291497e15fd7f9f2343ceaba5c1c1bc
Aljaz <aljaz.kodric1@student.um.si>
2024-01-03 13:31:05 +0100

    Implementacija GET-ID za zaposleni DELETE za zaposleni #WHA-16 DONE Time 2h

--- wha/src/main/java/si/feri/um/wha/dao/ZaposleniRepository.java
+++ wha/src/main/java/si/feri/um/wha/dao/ZaposleniRepository.java
@@ -2,9 +2,16 @@ package si.feri.um.wha.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import si.feri.um.wha.models.Artikel;
+import si.feri.um.wha.models.Tip_artikla;
 import si.feri.um.wha.models.Zaposleni;
 
 import java.util.List;
 
 public interface ZaposleniRepository extends CrudRepository<Zaposleni, Long> {
+
+    @Query("select z from Zaposleni z where z.ID_zaposleni = :id")
+    Zaposleni vrniDolocenegaZaposlenega(@Param("id") Long id);
+
 }
