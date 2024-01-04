83335d0fd538d295be0f11c3052a9742015a81c3
Aljaz <aljaz.kodric1@student.um.si>
2024-01-04 17:54:30 +0100

    Implementacija PUT in masovno dodajanje podatkov za Delovni_Cas in Zaposleni WHA-23 DONE Time 2h

--- wha/src/main/java/si/feri/um/wha/controllers/ZaposleniController.java
+++ wha/src/main/java/si/feri/um/wha/controllers/ZaposleniController.java
@@ -4,10 +4,13 @@ package si.feri.um.wha.controllers;
 import org.springframework.http.ResponseEntity;
 import si.feri.um.wha.dao.ZaposleniRepository;
 import si.feri.um.wha.models.Artikel;
+import si.feri.um.wha.models.Zaposleni;
 import si.feri.um.wha.models.Tip_artikla;
 import si.feri.um.wha.models.Zaposleni;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -35,6 +38,41 @@ public class ZaposleniController {
     public ResponseEntity<String> izbrisiZaposlenega(@PathVariable(name = "ID_zaposleni") Long ID_zaposleni) throws Exception {
         zaposleniDao.deleteById(ID_zaposleni);
         return ResponseEntity.ok("Uspesno izbrisan zaposleni.");
+    }
+
+    @PostMapping("/dodaj")
+    public ResponseEntity<String> dodajZaposlene(@RequestBody List<Zaposleni> zaposleni) {
+        Iterable<Zaposleni> savedZaposleni = zaposleniDao.saveAll(zaposleni);
+        return ResponseEntity.ok("Uspesno dodani zaposleni.");
+    }
+
+    @PutMapping("/posodobi/{ID_zaposleni}")
+    public ResponseEntity<String> posodobiZaposleni(@PathVariable(name = "ID_zaposleni") Long ID_zaposleni, @RequestBody Zaposleni updatedZaposleni) {
+        Zaposleni existingZaposleni = zaposleniDao.vrniDolocenegaZaposlenega(ID_zaposleni);
+
+        if (existingZaposleni == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (updatedZaposleni.getIme() != null) {
+            existingZaposleni.setIme(updatedZaposleni.getIme());
+        }
+        if (updatedZaposleni.getPriimek() != null) {
+            existingZaposleni.setPriimek(updatedZaposleni.getPriimek());
+        }
+        if (updatedZaposleni.getTelefon() != null) {
+            existingZaposleni.setTelefon(updatedZaposleni.getTelefon());
+        }
+        if (updatedZaposleni.getPlaca() != 0.0) {
+            existingZaposleni.setPlaca(updatedZaposleni.getPlaca());
+        }
+        if (updatedZaposleni.getTip_zaposlenega() != null) {
+            existingZaposleni.setTip_zaposlenega(updatedZaposleni.getTip_zaposlenega());
+        }
+
+        zaposleniDao.save(existingZaposleni);
+
+        return ResponseEntity.ok("Zaposleni uspešno posodobljen.");
     }
 
 
