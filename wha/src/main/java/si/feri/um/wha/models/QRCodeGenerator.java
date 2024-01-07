13bf7f6181c30645add66435a59b6265c2b85998
Aljaz <aljaz.kodric1@student.um.si>
2024-01-07 22:38:40 +0100

    Delujoc generator QR kode za Artikel object, potrebno je popraviti se filepath tako da bo delovalo za vse in poimenovanje qr-kod

---
+++ wha/src/main/java/si/feri/um/wha/models/QRCodeGenerator.java
@@ -0,0 +1,55 @@
+package si.feri.um.wha.models;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+
+public class QRCodeGenerator {
+
+    private static final String IMAGE_PATH = "C:/Users/aljaz/OneDrive/Dokumenti/FERI/RIS/Projekt1/qrcodes";
+    private static int imageNumber = 0;
+
+    public static void generateQRCodeImage(String text, int width, int height)
+            throws WriterException, IOException {
+        String fileName = getImageFileName();
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        String filePath = IMAGE_PATH + "/" + fileName;
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    private static String getImageFileName() {
+        String fileName = "QRCode.png";
+        if (imageNumber > 0) {
+            fileName = "QRCode" + imageNumber + ".png";
+        }
+        imageNumber++;
+        return fileName;
+    }
+
+
+
+    public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageConfig con = new MatrixToImageConfig( 0xFF000002 , 0xFFFFC041 ) ;
+
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream,con);
+        byte[] pngData = pngOutputStream.toByteArray();
+        return pngData;
+    }
+
+}
