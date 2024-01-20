379001ccff2555a0baffe546425c0f5855a78585
Aljaz <aljaz.kodric1@student.um.si>
2024-01-20 12:03:38 +0100

    delujoce

--- Frontend/frontend/src/pages/authentication/Narocila/dodajNarocilo.js
+++ Frontend/frontend/src/pages/authentication/Narocila/dodajNarocilo.js
@@ -5,8 +5,8 @@ import MainCard from 'components/MainCard';
 import Autocomplete from '@mui/material/Autocomplete';
 import api from '../../../services/api';
 import { format, addDays } from 'date-fns';
-
-
+ 
+ 
 const DodajNarocilo = () => {
   // ===================================================================================== STRANKA =========================================================================
     const [selectedStranka, setSelectedStranka] = useState(null); // Store the selected stranka object
@@ -14,22 +14,22 @@ const DodajNarocilo = () => {
     const [field3Value, setField3Value] = useState('');
     const [field4Value, setField4Value] = useState('');
     const [suggestions, setSuggestions] = useState([]);
-  
+ 
     useEffect(() => {
       const fetchSuggestions = async () => {
         try {
           const response = await api.get('/stranka');
           const data = response.data;
-  
+ 
           setSuggestions(data);
         } catch (error) {
           console.error('Error fetching suggestions:', error);
         }
       };
-  
+ 
       fetchSuggestions();
     }, []);
-  
+ 
     useEffect(() => {
       // Update Field 2, Field 3, and Field 4 when selectedStranka changes
       if (selectedStranka) {
@@ -43,35 +43,35 @@ const DodajNarocilo = () => {
         setField4Value('');
       }
     }, [selectedStranka]);
-  
+ 
     // ===================================================================================== ARTIKLI =========================================================================
     const [tableData, setTableData] = useState([
       { col1: '', col2: '', col3: '', col4: '', col5: '', col6:'', selectedArtikel: null },
     ]);
     const [suggestionsArtikli, setArtikliSuggestions] = useState([]);
-  
+ 
     const addRow = () => {
       setTableData([...tableData, { col1: '', col2: '', col3: '', col4: '', col5: '', col6:'', selectedArtikel: null }]);
     };
-  
+ 
     const handleArtikelChange = async (event, newValue, index) => {
       const updatedTableData = [...tableData];
-    
+   
       // Update selectedArtikel in the row
       updatedTableData[index].selectedArtikel = newValue;
       updatedTableData[index].col2 = newValue ? 1 : '';
-    
+   
       // Fetch additional data (dobavna cena and prodajna cena) for the selected artikel
       if (newValue) {
         try {
           const response = await api.get(`/artikli/${newValue.id_artikel}`);
           const { dobavnaCena, prodajnaCena, kolicina } = response.data;
-    
+   
           // Update dobavna cena and prodajna cena in the row
           updatedTableData[index].col3 = dobavnaCena.toString();
           updatedTableData[index].col4 = prodajnaCena.toString();
           updatedTableData[index].col6 = kolicina.toString();
-    
+   
           // Calculate and update skupaj based on prodajna cena and kolicina
           if (!isNaN(updatedTableData[index].col2)) {
             updatedTableData[index].col5 = (parseFloat(updatedTableData[index].col4) * parseFloat(updatedTableData[index].col2)).toFixed(2);
@@ -86,51 +86,51 @@ const DodajNarocilo = () => {
         updatedTableData[index].col5 = '';
         updatedTableData[index].col6 = '';
       }
-    
+   
       setTableData(updatedTableData);
     };
-  
+ 
     const handleKolicinaChange = (event, index) => {
       const updatedTableData = [...tableData];
-  
+ 
       // Update kolicina in the row
       updatedTableData[index].col2 = event.target.value;
-  
+ 
       // If prodajna cena is available, calculate and update skupaj
       if (!isNaN(updatedTableData[index].col2) && !isNaN(updatedTableData[index].col4)) {
         updatedTableData[index].col5 = (parseFloat(updatedTableData[index].col4) * parseFloat(updatedTableData[index].col2)).toFixed(2);
       }
-  
+ 
       setTableData(updatedTableData);
     };
-  
+ 
     useEffect(() => {
       const fetchArtikli = async () => {
         try {
           const response = await api.get('/artikli');
           const data = response.data;
-  
+ 
           setArtikliSuggestions(data);
         } catch (error) {
           console.error('Error fetching suggestions:', error);
         }
       };
-  
+ 
       fetchArtikli();
     }, []);
-
+ 
     // ===================================================================================== SPODNJI DEL =========================================================================
     const [bottomLeft1, setBottomLeft1] = useState('');
     const [bottomLeft2, setBottomLeft2] = useState('');
     const [bottomLeft3, setBottomLeft3] = useState('');
     const [skupajSum, setSkupajSum] = useState(0);
-
+ 
     useEffect(() => {
       const currentDate = new Date();
       const formattedDate = currentDate.toISOString().split('T')[0]; // ISO format without time
       setBottomLeft1(formattedDate);
     }, []);
-  
+ 
     // Effect to set "rok priprave" one day after Bottom Left 1
     useEffect(() => {
       if (bottomLeft1) {
@@ -140,20 +140,20 @@ const DodajNarocilo = () => {
         setBottomLeft2(formattedDate);
       }
     }, [bottomLeft1]);
-
+ 
     useEffect(() => {
       const sum = tableData.reduce((acc, row) => acc + parseFloat(row.col5 || 0), 0);
       setSkupajSum(sum);
     }, [tableData]);
-
+ 
     useEffect(() => {
       setBottomLeft3(skupajSum.toFixed(2));
     }, [skupajSum]);
-
+ 
     // ===================================================================================== SHRANJEVANJE =========================================================================
     const handleSave = async () => {
       const invalidRow = tableData.find(row => parseFloat(row.col2) > parseFloat(row.col6));
-
+ 
       if (invalidRow) {
         // Show an alert if there is an invalid row
         alert('The quantity in some rows is greater than "na zalogi". Please correct it before saving.');
@@ -163,10 +163,10 @@ const DodajNarocilo = () => {
         alert('Naziv Stranke must not be empty.');
         return; // Prevent saving
       }
-    
+   
       // Check if "Artikel" is empty in any row
       const emptyArtikel = tableData.find(row => !row.selectedArtikel);
-    
+   
       if (emptyArtikel) {
         alert('Artikel must not be empty in some rows. Please select an Artikel for each row.');
         return; // Prevent saving
@@ -196,7 +196,7 @@ const DodajNarocilo = () => {
           "id_zaposleni": 1
       }
       };
-  
+ 
       console.log(dataToSave)
       api.post(`/narocila`, dataToSave)
       .then((result) => {
@@ -206,8 +206,8 @@ const DodajNarocilo = () => {
         console.error('There was an error creating new narocilo!', error);
       });
     };
-
-    
+ 
+   
     const handleDownloadPDF = async () => {
       try {
         // Gather all the data needed for the PDF on the client side
@@ -218,15 +218,15 @@ const DodajNarocilo = () => {
           bottomLeft2,
           bottomLeft3,
         };
-
+ 
         console.log(clientData)
-    
+   
         // Send a POST request to generate the PDF with client data
         const response = await api.post('/generate-pdf', clientData, { responseType: 'arraybuffer' });
-    
+   
         // Create a Blob from the response data
         const blob = new Blob([response.data], { type: 'application/pdf' });
-    
+   
         // Create a download link and trigger the download
         const link = document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
@@ -236,9 +236,9 @@ const DodajNarocilo = () => {
         console.error('Error downloading PDF:', error);
       }
     };
-    
-
-  
+   
+ 
+ 
     return (
       <MainCard>
         <div style={{ marginBottom: '20px' }}>
@@ -263,7 +263,7 @@ const DodajNarocilo = () => {
               <TextField label="Kraj" value={field4Value} fullWidth />
             </Grid>
         </Grid>
-
+ 
         {/* Main Table-Like Div */}
         <Paper style={{ padding: '20px', marginTop: '20px' }}>
           {/* Table Header */}
@@ -287,7 +287,7 @@ const DodajNarocilo = () => {
               <Typography>Skupaj</Typography>
             </Grid>
           </Grid>
-
+ 
           {/* Table Rows */}
           {tableData.map((row, index) => (
             <Grid container spacing={2} key={index} style={{ marginBottom: '10px' }}>
@@ -321,13 +321,13 @@ const DodajNarocilo = () => {
               </Grid>
             </Grid>
           ))}
-
+ 
           {/* Plus Sign to Add Another Row */}
           <IconButton onClick={addRow} color="primary">
             <AddIcon />
           </IconButton>
         </Paper>
-
+ 
         {/* Bottom Left Corner Fields */}
         <Grid container spacing={2} style={{ marginTop: '20px' }}>
         <Grid item xs={4}>
@@ -346,7 +346,7 @@ const DodajNarocilo = () => {
             <TextField label="Cena skupaj:" value={bottomLeft3} fullWidth disabled />
           </Grid>
         </Grid>
-
+ 
         {/* Bottom Right Corner Buttons */}
         <Grid container spacing={2}  style={{ marginTop: '20px', justifyContent: 'flex-end' }}>
           <Grid item xs={12} style={{ display: 'flex', justifyContent: 'flex-end' }}>
@@ -362,5 +362,5 @@ const DodajNarocilo = () => {
     </MainCard>
   );
 };
-
+ 
 export default DodajNarocilo;
