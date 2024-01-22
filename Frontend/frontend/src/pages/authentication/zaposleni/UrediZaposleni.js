33a6b77911b1eb79b01fe22d80494497b4fcc169
Aljaz <aljaz.kodric1@student.um.si>
2024-01-22 16:01:39 +0100

    polepsava tabele pri zaposleni

--- Frontend/frontend/src/pages/authentication/zaposleni/UrediZaposleni.js
+++ Frontend/frontend/src/pages/authentication/zaposleni/UrediZaposleni.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { Button, TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import {
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import api from "../../../services/api";
-import Alert from '@mui/material/Alert';
+import Alert from "@mui/material/Alert";
 
 export default function UrediZaposleni({ userToEdit, fetchUser, onCancel }) {
   const [userData, setUserData] = useState({
-    ime: '',
-    priimek: '',
-    username: '',
-    password: '',
-    email: '',
-    placa: '',
-    telefon: '',
-    tip_zaposlenega: ''
+    ime: "",
+    priimek: "",
+    username: "",
+    password: "",
+    email: "",
+    placa: "",
+    telefon: "",
+    tip_zaposlenega: "",
   });
   const [showAlert, setShowAlert] = useState(false);
 
@@ -26,7 +33,7 @@ export default function UrediZaposleni({ userToEdit, fetchUser, onCancel }) {
         email: userToEdit.email,
         placa: userToEdit.placa,
         telefon: userToEdit.telefon,
-        tip_zaposlenega: userToEdit.tip_zaposlenega
+        tip_zaposlenega: userToEdit.tip_zaposlenega,
       });
     }
   }, [userToEdit]);
@@ -46,7 +53,7 @@ export default function UrediZaposleni({ userToEdit, fetchUser, onCancel }) {
         onCancel(); // Close the edit form
       })
       .catch((error) => {
-        console.error('There was an error updating the user!', error);
+        console.error("There was an error updating the user!", error);
       });
   };
 
@@ -56,72 +63,90 @@ export default function UrediZaposleni({ userToEdit, fetchUser, onCancel }) {
   };
 
   const alertStyle = {
-    position: 'fixed',
+    position: "fixed",
     top: 15,
-    left: '50%',
-    transform: 'translateX(-50%)',
+    left: "50%",
+    transform: "translateX(-50%)",
     zIndex: 1000,
-    margin: '0 auto',
-    width: '80%',
+    margin: "0 auto",
+    width: "80%",
     opacity: 0.95,
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
   };
 
   return (
     <>
-      {showAlert && <Alert  style={alertStyle} severity="success">Zaposleni uspešno posodobljen!</Alert>}
+      {showAlert && (
+        <Alert style={alertStyle} severity="success">
+          Zaposleni uspešno posodobljen!
+        </Alert>
+      )}
       <form onSubmit={handleUpdateUser}>
         <TextField
-          name="ime"
           label="Ime"
-          variant="standard"
+          name="ime"
+          variant="outlined"
           onChange={handleChange}
           value={userData.ime}
+          fullWidth
+          margin="normal"
         />
         <TextField
-          name="priimek"
           label="Priimek"
-          variant="standard"
+          name="priimek"
+          variant="outlined"
           onChange={handleChange}
           value={userData.priimek}
+          fullWidth
+          margin="normal"
         />
         <TextField
-          name="username"
           label="Uporabniško ime"
-          variant="standard"
+          name="username"
+          variant="outlined"
           onChange={handleChange}
           value={userData.username}
+          fullWidth
+          margin="normal"
         />
         <TextField
-          name="password"
           label="Geslo"
+          name="password"
           type="password"
-          variant="standard"
+          variant="outlined"
           onChange={handleChange}
           value={userData.password}
+          fullWidth
+          margin="normal"
         />
         <TextField
-          name="email"
           label="E-pošta"
-          variant="standard"
+          name="email"
+          variant="outlined"
           onChange={handleChange}
           value={userData.email}
+          fullWidth
+          margin="normal"
         />
         <TextField
-          name="placa"
           label="Plača"
-          variant="standard"
+          name="placa"
+          variant="outlined"
           onChange={handleChange}
           value={userData.placa}
+          fullWidth
+          margin="normal"
         />
         <TextField
-          name="telefon"
           label="Telefon"
-          variant="standard"
+          name="telefon"
+          variant="outlined"
           onChange={handleChange}
           value={userData.telefon}
+          fullWidth
+          margin="normal"
         />
-        <FormControl fullWidth>
+        <FormControl fullWidth variant="outlined" margin="normal">
           <InputLabel id="role-select-label">Delovno Mesto</InputLabel>
           <Select
             labelId="role-select-label"
@@ -136,7 +161,12 @@ export default function UrediZaposleni({ userToEdit, fetchUser, onCancel }) {
             <MenuItem value="VODJA_SKLADISCA">Vodja skladišča</MenuItem>
           </Select>
         </FormControl>
-        <Button type="submit" variant="contained">
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={{ marginTop: "20px" }}
+        >
           Posodobi
         </Button>
       </form>
--- Frontend/frontend/src/pages/authentication/zaposleni/UserForm.js
+++ Frontend/frontend/src/pages/authentication/zaposleni/UserForm.js
@@ -1,99 +1,170 @@
 import React, { useEffect, useState } from "react";
-import { Button, TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import {
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import api from "../../../services/api";
-import Alert from '@mui/material/Alert';
+import Alert from "@mui/material/Alert";
 
+export default function UserForm({ fetchUser }) {
+  const [userData, setUserData] = useState({
+    ime: "",
+    priimek: "",
+    username: "",
+    password: "",
+    email: "",
+    placa: "",
+    telefon: "",
+    tip_zaposlenega: "",
+  });
+  const [showAlert, setShowAlert] = useState(false);
 
+  const handleChange = (event) => {
+    setUserData({ ...userData, [event.target.name]: event.target.value });
+  };
 
-export default function UserForm({fetchUser}) {
-   const [userData, setUserData] = useState({
-      ime: '',
-      priimek: '',
-      username: '',
-      password: '',
-      email: '',
-      placa: '',
-      telefon: '',
-      tip_zaposlenega: ''
-   });
-   const [showAlert, setShowAlert] = useState(false);
+  const createUser = () => {
+    console.log(userData);
+    api
+      .post(`/zaposleni`, userData)
+      .then((result) => {
+        console.log(result.data);
+        fetchUser();
+      })
+      .catch((error) => {
+        console.error("There was an error creating new user!", error);
+      });
+  };
 
-   const handleChange = (event) => {
-      setUserData({ ...userData, [event.target.name]: event.target.value });
-   };
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    createUser();
+    fetchUser();
+  };
 
-   const createUser = () => {
-      console.log(userData)
-      api.post(`/zaposleni`, userData)
-        .then((result) => {
-          console.log(result.data);
-          fetchUser();
-        })
-        .catch((error) => {
-          console.error('There was an error creating new user!', error);
-        });
-   };
+  const [user, setUsers] = useState([]);
 
-   const handleCreateUser = (event) => {
-      event.preventDefault();
-      createUser();
-      fetchUser();
-   };
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-   const [user, setUsers] = useState([]);
-
-   useEffect(() => {
-      fetchUser();
-   }, []);
-
-
-   const showDeleteAlert = () => {
-      setShowAlert(true);
-      setTimeout(() => setShowAlert(false), 3000);
+  const showDeleteAlert = () => {
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 3000);
   };
 
   const alertStyle = {
-     position: 'fixed', // Fixed position
-     top: 15,          // 10px from the top
-     left: '50%',      // Centered horizontally
-     transform: 'translateX(-50%)', // Adjust for centering
-     zIndex: 1000,     // Ensure it's above other elements
-     margin: '0 auto', // Centering for smaller screens
-     width: '80%',      // Responsive width
-     opacity: 0.95,  // 85% opacity
-     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
- };
+    position: "fixed", // Fixed position
+    top: 15, // 10px from the top
+    left: "50%", // Centered horizontally
+    transform: "translateX(-50%)", // Adjust for centering
+    zIndex: 1000, // Ensure it's above other elements
+    margin: "0 auto", // Centering for smaller screens
+    width: "80%", // Responsive width
+    opacity: 0.95, // 85% opacity
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+  };
 
-
-   return (
-      <>
-         {showAlert && <Alert  style={alertStyle} severity="success">Zaposleni uspešno izbrisan!</Alert>}
-         <form onSubmit={handleCreateUser}>
-            <TextField name="ime" label="Ime" variant="standard" onChange={handleChange} value={userData.ime} />
-            <TextField name="priimek" label="Priimek" variant="standard" onChange={handleChange} value={userData.priimek} />
-            <TextField name="username" label="Uporabniško ime" variant="standard" onChange={handleChange} value={userData.username} />
-            <TextField name="password" label="Geslo" type="password" variant="standard" onChange={handleChange} value={userData.password} />
-            <TextField name="email" label="E-pošta" variant="standard" onChange={handleChange} value={userData.email} />
-            <TextField name="placa" label="Plača" variant="standard" onChange={handleChange} value={userData.placa} />
-            <TextField name="telefon" label="Telefon" variant="standard" onChange={handleChange} value={userData.telefon} />
-            <FormControl fullWidth>
-               <InputLabel id="role-select-label">Delovno Mesto</InputLabel>
-               <Select
-                  labelId="role-select-label"
-                  name="tip_zaposlenega"
-                  value={userData.tip_zaposlenega}
-                  label="Delovno Mesto"
-                  onChange={handleChange}
-               >
-                  <MenuItem value="VODJA_PODJETJA">Vodja podjetja</MenuItem>
-                  <MenuItem value="DOKUMENTARIST">Dokumentarist</MenuItem>
-                  <MenuItem value="SKLADISCNIK">Skladiščnik</MenuItem>
-                  <MenuItem value="VODJA_SKLADISCA">Vodja skladišča</MenuItem>
-               </Select>
-            </FormControl>
-            <Button type="submit" variant="contained">Ustvari</Button>
-         </form>
-      </>
-   );
-};
-
+  return (
+    <>
+      {showAlert && (
+        <Alert style={alertStyle} severity="success">
+          Zaposleni uspešno izbrisan!
+        </Alert>
+      )}
+      <form onSubmit={handleCreateUser}>
+        <TextField
+          label="Ime"
+          name="ime"
+          variant="outlined"
+          onChange={handleChange}
+          value={userData.ime}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Priimek"
+          name="priimek"
+          variant="outlined"
+          onChange={handleChange}
+          value={userData.priimek}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Uporabniško ime"
+          name="username"
+          variant="outlined"
+          onChange={handleChange}
+          value={userData.username}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Geslo"
+          name="password"
+          type="password"
+          variant="outlined"
+          onChange={handleChange}
+          value={userData.password}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="E-pošta"
+          name="email"
+          variant="outlined"
+          onChange={handleChange}
+          value={userData.email}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Plača"
+          name="placa"
+          variant="outlined"
+          onChange={handleChange}
+          value={userData.placa}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Telefon"
+          name="telefon"
+          variant="outlined"
+          onChange={handleChange}
+          value={userData.telefon}
+          fullWidth
+          margin="normal"
+        />
+        <FormControl fullWidth variant="outlined" margin="normal">
+          <InputLabel id="role-select-label">Delovno Mesto</InputLabel>
+          <Select
+            labelId="role-select-label"
+            name="tip_zaposlenega"
+            value={userData.tip_zaposlenega}
+            label="Delovno Mesto"
+            onChange={handleChange}
+          >
+            <MenuItem value="VODJA_PODJETJA">Vodja podjetja</MenuItem>
+            <MenuItem value="DOKUMENTARIST">Dokumentarist</MenuItem>
+            <MenuItem value="SKLADISCNIK">Skladiščnik</MenuItem>
+            <MenuItem value="VODJA_SKLADISCA">Vodja skladišča</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={{ marginTop: "20px" }}
+        >
+          Ustvari
+        </Button>
+      </form>
+    </>
+  );
+}
