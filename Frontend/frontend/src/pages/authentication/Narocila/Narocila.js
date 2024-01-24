21d7bca6a3c09b10043c6914182df40c7ab67daf
Aljaz <aljaz.kodric1@student.um.si>
2024-01-24 09:39:47 +0100

    narocila.js close icon

--- Frontend/frontend/src/pages/authentication/Narocila/Narocila.js
+++ Frontend/frontend/src/pages/authentication/Narocila/Narocila.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import NarocilaTable from "./NarocilaTable";
 import Alert from '@mui/material/Alert';
 import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
 import MainCard from '../../../components/MainCard';
 import { Button } from "@mui/material";
 import DodajNarocilo from './dodajNarocilo';
